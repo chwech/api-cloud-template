c9488a9799783f637344f1d46f6c9731623fae66
陈伟成 <804699297@qq.com>
2019-09-04 09:01:13 +0800

    支持less，stylus

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,59 @@
-'use strict';
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-const path = require('path')
-const glob = require("glob")
-const VueLoaderPlugin = require('vue-loader/lib/plugin')
+"use strict";
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const path = require("path");
+const glob = require("glob");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
 
-const debug = process.argv.indexOf('-d') !== -1
+const debug = process.argv.indexOf("-d") !== -1;
 
 let plugins = [
   new UglifyJsPlugin({
     uglifyOptions: {
       output: {
-          comments: false,
+        comments: false
       }
     }
   }),
   new VueLoaderPlugin() // 使用vue-loader必须使用这个插件
-]
+];
 
-let globmaths =  glob.sync("./src/pages/*.js",{
-  nodir:true,
-})
+let globmaths = glob.sync("./src/pages/*.js", {
+  nodir: true
+});
 
-let entry = globmaths.reduce((obj,file)=>{
-  let filename = path.basename(file).split(".")[0]
-  obj[filename] = ["babel-polyfill",file]
-  return obj
-},{})
+let entry = globmaths.reduce((obj, file) => {
+  let filename = path.basename(file).split(".")[0];
+  obj[filename] = ["babel-polyfill", file];
+  return obj;
+}, {});
 
 module.exports = {
-  mode: 'production',
-    entry:entry,
-    output: {
-      path: path.resolve(`./lib`),
-      filename: "[name].js",
-    },
-    module: {
-      rules: [
-        { test: /\.js$/, exclude: /node_modules/, use: ["babel-loader"] },
-        {
-          test: /\.vue$/,
-          loader: 'vue-loader'
-        }
-      ]
-    },
-    plugins: plugins,
+  mode: "production",
+  entry: entry,
+  output: {
+    path: path.resolve(`./lib`),
+    filename: "[name].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: file => /node_modules/.test(file) && !/\.vue\.js/.test(file),
+        use: ["babel-loader"]
+      },
+      {
+        test: /\.vue$/,
+        loader: "vue-loader"
+      },
+      {
+        test: /\.less$/,
+        use: ["vue-style-loader", "css-loader", "less-loader"]
+      },
+      {
+        test: /\.styl(us)?$/,
+        use: ["vue-style-loader", "css-loader", "stylus-loader"]
+      }
+    ]
+  },
+  plugins: plugins
 };
